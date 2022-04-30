4c9a6fe30a71a5a02e0bfea75563cdf4e60490a3
LeAries <learies@ya.ru>
2022-04-30 05:27:43 +0300

    Added get secret key out dotenv or generate new secret key

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,8 +1,14 @@
 import os
 
+from django.core.management.utils import get_random_secret_key
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.getenv('SECRET_KEY') or get_random_secret_key()
 
 DEBUG = True
 
