67998b47f5221177b97f9cca50ed28262501d134
Artur Avtukhov <archy6754267@gmail.com>
2022-05-01 23:37:54 +0200

    начал добавлять views, serializers, urls __002

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,14 +4,14 @@ from reviews.models import Title, Genre, Category
 from api.serializers import TitleSerializer, CategorySerializer, GenreSerializer
 
 
-class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-class TitleViewSet(viewsets.ReadOnlyModelViewSet):
+class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
 
-class GenreViewSet(viewsets.ReadOnlyModelViewSet):
+class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
