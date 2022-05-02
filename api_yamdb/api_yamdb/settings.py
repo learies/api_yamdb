f505ed95e9e1c8172211de7bb8d083f736c7064a
Сергей Соловьев <ser5olovjew@yandex.ru>
2022-05-02 15:36:22 +0300

    Добавлена модель User

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -79,6 +79,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.User'
 
 LANGUAGE_CODE = 'en-us'
 
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+from .managers import CustomUserManager
+
+USER = 'user'
+MODERATOR = 'moderator'
+ADMIN = 'admin'
+
+class User(AbstractBaseUser):
+    USER_ROLE = [
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin')
+    ]
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+    )
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    bio = models.TextField(blank=True)
+    role = models.CharField(
+        max_length=150,
+        choices=USER_ROLE,
+        default=USER,
+        blank=True
+    )
+    # is_admin = models.BooleanField(default=False)
+    # is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email',]
+    objects = CustomUserManager()
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+    
+    def __str__(self):
+        return self.username
