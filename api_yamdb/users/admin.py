cc8fa23108f28e9e2aec60dc4992f004a91493f4
LeAries <learies@ya.ru>
2022-05-02 17:55:17 +0300

    Добавил в admin поля для users

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email', 'role')
