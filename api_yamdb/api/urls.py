ea6fba489471f13e2f4f85642f14f8147cfca960
LeAries <learies@ya.ru>
2022-05-03 21:08:07 +0300

    Поправил url на categories

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,8 +4,9 @@ from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, GenreViewSet, TitleViewSet, UserViewSet
 
 router_v1 = DefaultRouter()
+
 router_v1.register('users', UserViewSet)
-router_v1.register('category', CategoryViewSet)
+router_v1.register('categories', CategoryViewSet)
 router_v1.register('genre', GenreViewSet)
 router_v1.register('title', TitleViewSet)
 
