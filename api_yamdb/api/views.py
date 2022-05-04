5126ed4ca27477de97ef5a947aab5c4bbf4c581b
LeAries <learies@ya.ru>
2022-05-04 21:54:40 +0300

    Refact action me in UserViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -28,14 +28,13 @@ class UserViewSet(viewsets.ModelViewSet):
     )
     def me(self, request):
         user = get_object_or_404(User, username=self.request.user)
-        if request.method == 'GET':
-            serializer = MeSerializer(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
         if request.method == 'PATCH':
             serializer = MeSerializer(user, data=request.data, partial=True)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = MeSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
