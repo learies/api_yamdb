3b27fbf63f2bcc09e48e0bd30fdf3996573b287e
LeAries <learies@ya.ru>
2022-05-07 09:11:42 +0300

    Исправил ошибки в тестах для TitleViewSet

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -29,3 +29,23 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             request.method in permissions.SAFE_METHODS
             or (request.user.is_authenticated and request.user.is_admin)
         )
+
+
+class UserAndModeratorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or (
+                request.user.is_authenticated
+                and (
+                    obj.author == request.user
+                    or request.user.is_moderator
+                )
+            )
+        )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -13,7 +13,8 @@ from users.models import User
 
 from .mixins import ReviewGenreModelMixin
 from .paginations import PageNumberPagination
-from .permissions import AdminOnly, IsAdminOrReadOnly
+from .permissions import (AdminOnly, IsAdminOrReadOnly,
+                          UserAndModeratorOrReadOnly)
 from .serializers import (CategorySerializer, CommentSerializer,
                           GenreSerializer, ReviewSerializer, SignUpSerializer,
                           TitleSerializer, TitlesViewSerializer,
@@ -138,20 +139,22 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (UserAndModeratorOrReadOnly,)
 
     def get_queryset(self):
-        review = get_object_or_404(
-            Review,
-            id=self.kwargs.get('review_id')
-        )
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        try:
+            review = title.reviews.get(id=self.kwargs.get('review_id'))
+        except TypeError:
+            TypeError('Нет такого отзыва')
         return review.comments.all()
 
     def perform_create(self, serializer):
-        review = get_object_or_404(
-            Review,
-            id=self.kwargs.get('review_id')
-        )
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        try:
+            review = title.reviews.get(id=self.kwargs.get('review_id'))
+        except TypeError:
+            TypeError('Нет такого отзыва')
         serializer.save(author=self.request.user, review=review)
 
 
