62755d014c7cbdfe2f40c38e48853df972b6b5c7
LeAries <learies@ya.ru>
2022-05-15 16:34:40 +0300

    Delete ExtendedReadOnlyPermission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,11 +1,6 @@
 from rest_framework import permissions
 
 
-class ExtendedReadOnlyPermission(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS
-
-
 class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_admin or request.user.is_staff
